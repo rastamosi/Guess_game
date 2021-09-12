26609d7ac032c1a78bcc58cefd69d36db484dbea
Mostafa Ghorbani <48104432+rastamosi@users.noreply.github.com>
2021-09-12 10:10:42 +0300

    Guess game
    Guess the number that will be printed! any number from 1 to 10 .

---
+++ program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+namespace train
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+          var number = new Random().Next(1, 10);
+
+             
+            for (var i = 0; i < 4; i++)
+            {
+                Console.Write("Guess the secret number: ");
+                var guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    Console.WriteLine("You won!");
+                    return;
+                }
+            }
+            Console.WriteLine("Secret is " + number);
+            Console.WriteLine("You lost!");
+        }
+
+
+    }
+
+}
+
+
+ 
